75e13b3094812810ada022fc55ea9bab43898dcd
costa_rica <nrodrig1@gmail.com>
2020-01-13 13:43:42 -0500

    i think i've made a mess

--- featherlook1.0.py
+++ featherlook1.0.py
@@ -8,7 +8,7 @@ import os
 import glob
 
 # *********************************#
-# updated by costa_rica 13 Jan 2020#
+# updated by costa_rica 12 Jan 2020#
 # *********************************#
 
 def end_app():
@@ -19,6 +19,36 @@ def click():
     StoreVarFile=open(TmpFlNm, "w+")
     StoreVarFile.write(entry_1.get())
     Text_input_window.destroy()
+# make function where search_word is the parameter
+# parameters for function srch_wd = the string searching for in the file,
+# Dir_1 = the directory path to search for word files,
+# Fl_Typ = could be file name but intended to use a wildcard + "." + extension,
+# srch_lst= list where sentences will get stored
+# fl_lst = list var where file paths are stored will be stored
+# fl_list needs another function to parse out the file name from the path or there is a functino for just the file names
+def function_1(srch_wd, Dir_1, Fl_Typ, srch_lst, fl_lst):
+    for x in glob.glob(Dir_1 +"/"+Fl_Typ):
+        try:
+            paragraph_list = docx2txt.process(x).splitlines()
+        except:
+            paragraph_list = (f"searched for: {srch_wd} but file could not be searched due to formatting")
+    # search each file in directory for search_word. If in file append only the sentence and file name to corresponding lists
+        for i in paragraph_list:
+            if i.find(srch_wd)>=0:
+                # parse paragraph for the sentence the word is in
+                if i.count(".")== 0 or i.count(".")==1:
+                    srch_lst.append(i)
+                    fl_lst.append(os.path.basename(x))
+                else:
+                    while i.count(".") > 1:
+                        ChrCnt = i.find(".")
+                        SntncTmp = i[0: ChrCnt + 1]
+                        if SntncTmp.count(srch_wd) > 0:
+                            srch_lst.append(SntncTmp)
+                            fl_lst.append(os.path.basename(x))
+                            i = i[ChrCnt + 1:].strip()
+                        else:
+                            i = i[ChrCnt + 1:].strip()
 
 # getting path from .exe
 if getattr(sys, 'frozen', False):
@@ -41,10 +71,14 @@ label_1.grid(row=1, column=0, sticky=W)
 entry_1=ttk.Entry(Text_input_window, width=40, background="white")
 entry_1.grid(row=2, column=0, sticky=W)
 TmpFlNm="StoreVar_" + ApplctnNm + ".txt"
+
+
 btn_1=ttk.Button(Text_input_window, text="SUBMIT", width=10, command=click)
 btn_1.grid(row=3, column=1, sticky=W)
 Text_input_window.mainloop()
 
+search_list=[]
+file_list=[]
 
 root = Tk()
 # .withdraw() method hides the window that is used to do the .askdirectory() method.
@@ -55,78 +89,33 @@ search_dir = os.path.dirname(root.fileName)
 root.destroy()
 root.mainloop()
 
-# # not sure i need the line below:
-# Store_1=feath_dir +"/"+ TmpFlNm
+# Store_1=feath_dir.replace('\\','//') +"/"+ TmpFlNm
+Store_1=feath_dir +"/"+ TmpFlNm
 
+
+# make new window to tell me paths and names
+path_names_window=Tk()
+path_names_window.title("See path names")
+path_names_window.geometry('300x300+70+400')
+# text_1=Text(path_names_window,width=100, height=20, wrap='word')
+label_2=ttk.Label(path_names_window,text=Store_1, font=("arial",10, "underline", "bold"))
+label_2.grid(row=1, column=1, sticky='w')
+path_names_window.mainloop()
+# search word is saved in this text file becuase i don't wnat to use the .get() method
 StoreVarFile_1=open(feath_dir.replace('\\','//') +"/"+ TmpFlNm,"r")
 # StoreVarFile_1=open(feath_dir +"/"+ TmpFlNm,"r")
 # print(StoreVarFile_1)
 Store_1=feath_dir.replace('\\','//') +"/"+ TmpFlNm
 search_word= StoreVarFile_1.readline()
 StoreVarFile_1.close()
-
-# variables and things needed for file and word parser
-search_list=[]
-file_list=[]
-x = int()
-y = int()
-file_counter=int()
-file_limit=2
-paragraph_limit=3
-para_sentence_limit=4
+x=int()
 
 
-# ******put file word search and parser here*************
-for a in glob.glob(search_dir+"/"+"*.docx"):
-    # print('are we here?')
-    para_counter=0
-    file_counter+=1
-    if a.count(search_word)>0:
-        paragraph_list=docx2txt.process(a)
-        # b is each paragraph in a file
-        for b in paragraph_list.split('\n'):
-            b_sentence_count = 0
-            para_counter+=1
-            # print(file_counter, para_counter, b_sentence_count)
-            if b.count(search_word)>0:
-                if b.count(".")<=1:
-                    file_list.append(os.path.basename(a))
-                    search_list.append(b)
-                    x=len(file_list)
-                    y=len(search_list)
-                # establishes the paragraph has more than one sentence with elif below
-                elif b.count(".")>1:
-                    while b.count(".")>0:
-                        # print("elif b.count(.)>1 is " + str(b.count(".")))
-                        b_subsentence = b[0:b.find(".") +1]
-                        b_sentence_count +=1
-                        # establishes the search_word is not in b_subsentence
-                        if b_subsentence.count(search_word) == 0:
-                            # make b everything b was except b_subsentce
-                            b = b[len(b_subsentence):]
-                        # search_word is in b_subsentence
-                        elif b_subsentence.count(search_word)>0:
-                            search_list.append(b_subsentence)
-                            file_list.append(os.path.basename(a))
-                            x = len(file_list)
-                            y = len(search_list)
-                            b = b[len(b_subsentence):]
-                            # print(file_list[x - 1], x, search_list[y - 1], para_counter)
-                            # ***********LIMIT number of SENTENCES in paragraph***********
-                            if b_sentence_count >= para_sentence_limit:
-                                break
-            # ***********LIMIT number of PARAGRAPHS in file***********
-            elif para_counter>=paragraph_limit:
-                # print('this filed_ 2')
-                break
-        # ***********LIMIT number of Files***********
-        if file_counter>=file_limit:
-            break
+# call Search_Dir_for_word function
+function_1(search_word, search_dir, "*.docx",search_list, file_list)
 
-
-
-# print(y)
 InfoWindow = Tk()
+
 InfoWindow.title("Searched for:")
 Close_Button = ttk.Button(InfoWindow,text="Close",command=InfoWindow.destroy)
 Close_Button.grid(row=len(search_list)+3, column=2, sticky='w')
@@ -147,7 +136,9 @@ for i in range(len(file_list)):
         search_list_dict[y] = ttk.Label(InfoWindow, text=search_list[y]).grid(row=2+y, column=4, sticky='w')
 
 # *************** delete temporary text file here
+# feath_dir="D:\Documents\App development\\featherlook\dist"
 file_to_remove = feath_dir+'/' + TmpFlNm
+# print(file_to_remove)
 os.remove(file_to_remove)
 
 InfoWindow.mainloop()
